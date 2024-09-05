9da1b973033e1fdc709187894c7623e85deeb48d
GMina356 <www.napilmena50@gmail.com>
2024-09-06 02:03:40 +0300

    upload T2 answers

---
+++ Week 07/2. Reverse nodes func/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "List.h"
+
+void display(char c)
+{
+    printf("%c, ", c);
+}
+
+void ReverseList(List *pl)
+{
+    char n;
+    List lt;
+    CreatList(&lt);
+    while(!ListEmpty(pl))
+    {
+        DeleteList(0, &n, pl);
+        InsertList(0, &n, &lt);
+    }
+    *pl = lt;
+}
+
+int main()
+{
+    List l;
+    CreatList(&l);
+    char n[] = "abcd";
+    InsertList(0, &n[0], &l);
+    InsertList(1, &n[1], &l);
+    InsertList(2, &n[2], &l);
+    InsertList(3, &n[3], &l);
+    printf("\nbefore reverse : ");
+    TraverseList(&l, &display);
+    ReverseList(&l);
+    printf("\nafter reverse :  ");
+    TraverseList(&l, &display);
+    printf("\n");
+    return 0;
+}
